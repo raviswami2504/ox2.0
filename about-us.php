8213d4ed4e3ab35a03b30e150995e1fa4b7d8eae
Himanshu chauhan <164041418+Himanshu-chauhan15@users.noreply.github.com>
2025-05-28 18:27:56 +0530

    all done

--- about-us.php
+++ about-us.php
@@ -16,36 +16,7 @@
   <link rel="stylesheet" href="./assets/css/colors/purple.css" />
 
   <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y6M0T9NLP4"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag("js", new Date());
-
-    gtag("config", "G-Y6M0T9NLP4");
-  </script>
-  <style>
-    .btn-primary:hover {
-      background-color: #fab758 !important;
-      border-color: #fab758 !important;
-    }
-
-    footer a:hover {
-      color: #fab758 !important;
-    }
-
-    #phone-content h1 {
-      font-size: 18px;
-      color: white;
-    }
-
-    #number-content a {
-      font-size: 18px;
-      color: #60697b;
-    }
-  </style>
+ <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
